615e31a64280f0a1fd0039e94b725292d5047246
Aaron Wiggins <AaronWiggins@Aarons-MacBook-Pro.local>
2015-05-05 14:19:01 -0400

    simple == or != loop working

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,10 +1,12 @@
 def random_number
   rand(0..100)
 end
-puts random_number.to_i
+
+num = random_number
+puts num
 puts "guess a number btw 0 and 100"
 user_guess = gets.chomp.to_i
-  if user_guess == random_number
+  if user_guess == num
     puts "you got it"
   else
     puts "keep guessing"
