f7dfb96199891dc37e026beaef0419686261bb70
Aaron Wiggins <AaronWiggins@Aarons-MacBook-Pro.local>
2015-05-05 15:33:45 -0400

    Guess <, >, and == works. stops each if ==

--- guessing_game.rb
+++ guessing_game.rb
@@ -7,13 +7,13 @@ puts num
 (1..5).each do |b|
   puts "guess a number btw 0 and 100"
   user_guess = gets.chomp.to_i
-  if user_guess == num
-    puts "you got it"
-  elsif user_guess > num
+
+  if user_guess > num
     puts "you're too high"
   elsif user_guess < num
     puts "you're too low"
-  # else
-  #   puts "keep guessing"
+  elsif user_guess == num
+    puts "that's right"
+    exit
   end
 end
