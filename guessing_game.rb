d7018cd559cdbcedcf9df3e066a3fa597e1b9f4d
Aaron Wiggins <AaronWiggins@Aarons-MacBook-Pro.local>
2015-05-05 16:30:52 -0400

    added an array to store user input

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,19 +1,21 @@
-def random_number
-  rand(0..100)
-end
+ def random_number
+   rand(0..100)
+ end
 
-num = random_number
-puts num
-(1..5).each do |b|
-  puts "guess a number btw 0 and 100"
-  user_guess = gets.chomp.to_i
-
-  if user_guess > num
-    puts "you're too high"
-  elsif user_guess < num
-    puts "you're too low"
-  elsif user_guess == num
-    puts "that's right"
-    exit
-  end
-end
+ num = random_number
+ array = []
+ puts num
+ (1..5).each do |b|
+   puts "guess a number btw 0 and 100"
+   user_guess = gets.chomp.to_i
+   array << user_guess
+   p array
+   if user_guess == num
+     puts "that's right"
+     break
+   elsif user_guess < num
+     puts "you're too low"
+   elsif user_guess > num
+     puts "you're too high"
+   end
+ end
