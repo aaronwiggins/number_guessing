c3c3df190de8b766e7cf903fdee26b387bee2c73
Aaron Wiggins <AaronWiggins@Aarons-MacBook-Pro.local>
2015-05-05 14:40:27 -0400

    each loop running 5 times

--- guessing_game.rb
+++ guessing_game.rb
@@ -4,10 +4,16 @@ end
 
 num = random_number
 puts num
-puts "guess a number btw 0 and 100"
-user_guess = gets.chomp.to_i
+(1..5).each do |b|
+  puts "guess a number btw 0 and 100"
+  user_guess = gets.chomp.to_i
   if user_guess == num
     puts "you got it"
-  else
-    puts "keep guessing"
+  elsif user_guess > num
+    puts "you're too high"
+  elsif user_guess < num
+    puts "you're too low"
+  # else
+  #   puts "keep guessing"
   end
+end
