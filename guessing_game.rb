7f91022e1cf53fd8ad1f1a3462e282badf1f34cc
Aaron Wiggins <AaronWiggins@Aarons-MacBook-Pro.local>
2015-05-05 13:49:50 -0400

    loop, if user input is the same as random number

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,6 +1,11 @@
 def random_number
   rand(0..100)
 end
-random_number
+puts random_number.to_i
 puts "guess a number btw 0 and 100"
 user_guess = gets.chomp.to_i
+  if user_guess == random_number
+    puts "you got it"
+  else
+    puts "keep guessing"
+  end
